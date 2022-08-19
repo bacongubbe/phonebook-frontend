d2d6ac1352219b483045b3ad87a5bdb61701590b
bacongubbe <berg.felix@hotmail.com>
2022-08-19 17:53:38 +0200

    .

--- functions.js
+++ functions.js
@@ -1,19 +1,32 @@
 function getContacts(element) {
-    fetch ("http://localhost:8080/contacts")
-        .then ((response) => response.json)
-        .then ((data) => {
-            const contactList = createContactList(data.results);
+    fetch("http://localhost:8080/contacts", {
+        mode: 'no-cors'
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            const contactList = createContactList(data);
             element.appendChild(contactList);
-        })
+        });
 }
 
 function createContactList(contacts) {
-    const contactList = document.createElement("div");
+    const contactList = document.createElement('div');
 
-    for (let i = 0; contacts.length < i; i++) {
-        const contactDiv = document.createElement("div");
+    for (let i = 0; i < contacts.length; i++) {
+        const contactDiv = document.createElement('div');
         contactDiv.className = "contact";
 
-        
+        contactDiv.appendChild(createContactName(contacts[i]));
+        contactDiv.appendChild();
+        contactDiv.appendChild();
+
     }
 }
+
+function createContactName (contact){
+    const contactName = document.createElement('h3');
+    contactName.textContent = `${contact.name}`;
+
+    return contactName;
+}
+
