2f3d0bbdf8481dcc9b7341f48e399c54f342c001
bacongubbe <berg.felix@hotmail.com>
2022-08-20 16:31:27 +0200

    adding the connection to API

--- functions.js
+++ functions.js
@@ -1,32 +1,55 @@
 function getContacts(element) {
-    fetch("http://localhost:8080/contacts", {
-        mode: 'no-cors'
-    })
-        .then((response) => response.json())
-        .then((data) => {
-            const contactList = createContactList(data);
-            element.appendChild(contactList);
-        });
-}
+    fetch("http://localhost:8080/contacts")
+      .then((response) => response.json())
+      .then((data) => {
+        const contactList = createContactList(data);
+        element.appendChild(contactList);
+      });
+  }
 
-function createContactList(contacts) {
-    const contactList = document.createElement('div');
+  function createContactList(contacts) {
+    const contactList = document.createElement("div");
+    contactList.className = "contactList";
 
     for (let i = 0; i < contacts.length; i++) {
-        const contactDiv = document.createElement('div');
-        contactDiv.className = "contact";
+      const contactDiv = document.createElement("div");
+      contactDiv.className = "contactDiv";
 
-        contactDiv.appendChild(createContactName(contacts[i]));
-        contactDiv.appendChild();
-        contactDiv.appendChild();
+      contactDiv.appendChild(createContactName(contacts[i]));
+      contactDiv.appendChild(createContactAddress(contacts[i]));
+      contactDiv.appendChild(createContactNumber(contacts[i]));
 
+      contactList.appendChild(contactDiv);
     }
-}
 
-function createContactName (contact){
-    const contactName = document.createElement('h3');
-    contactName.textContent = `${contact.name}`;
+    return contactList;
+  }
+
+  function createContactName(contacts) {
+    const contactName = document.createElement("h3");
+    contactName.className = "contactName";
+    contactName.textContent = `${contacts.name}`;
 
     return contactName;
-}
+  }
 
+  function createContactAddress(contacts){
+    const contactAddress = document.createElement('h4');
+    contactAddress.className = "contactAddress";
+    contactAddress.textContent = `${contacts.address}`;
+
+    return contactAddress;
+  }
+
+  function createContactNumber(contacts){
+    const contactNumber = document.createElement('h4');
+    contactNumber.className = "contactNumber";
+    contactNumber.textContent = `${contacts.phoneNumber}`;
+
+    return contactNumber;
+  }
+
+  document.addEventListener("DOMContentLoaded", () => {
+    const mainElement = document.querySelector("main");
+    getContacts(mainElement);
+  });
